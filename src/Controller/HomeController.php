cc8fbffd70611e4e2b3da8c5f904b3a4f6fb9987
bouare12 <bandiougoubouare@yahoo.fr>
2023-10-11 13:56:34 +0200

    Affichage article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+
+        //dd($articles);
+
         return $this->render('home/home.html.twig', [
             'hello_world' => 'Hello world!',
+            'articles' => $articles
         ]);
     }
 }
