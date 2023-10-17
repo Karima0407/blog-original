b63886c122e5e2abbd92d03c3d738d7aef895e0a
bouare12 <bandiougoubouare@yahoo.fr>
2023-10-17 14:25:34 +0200

    Correction TP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
     {
         $articles = $articleRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
         //dd($articles);
 
         return $this->render('home/home.html.twig', [
             'hello_world' => 'Hello world!',
-            'articles' => $articles
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 
@@ -29,6 +33,29 @@ class HomeController extends AbstractController
 
         return $this->render('home/show.html.twig', [
             'article' => $article
+        ]);
+    }
+
+    #[Route('/showArticleCategory/{id}', name: 'show_articles_category')]
+    public function showArticlesCategorie(Category $categorie, CategoryRepository $categoryRepository): Response
+    {
+
+        $categories = $categoryRepository->findAll();
+
+        if($categorie) {
+
+            $articles = $categorie->getArticles()->getValues();
+
+        }else {
+
+            return $this->redirectToRoute('app_home');
+        }
+        // $categorie = $categoryRepository->find($id);
+        //dd($articles);
+
+        return $this->render('home/home.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 }
